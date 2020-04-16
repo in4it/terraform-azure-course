68785a2149d301ee7cf6ec8932a4b5be6415ad4d
Edward Viaene <ward@in4it.io>
2020-04-16 10:54:58 +0200

    added depends on

--- network-security-groups/network.tf
+++ network-security-groups/network.tf
@@ -36,6 +36,8 @@ resource "azurerm_network_security_group" "internal-facing" {
     location            = var.location
     resource_group_name = azurerm_resource_group.demo.name
 
+    depends_on          = [azurerm_application_security_group.demo-instance-group]
+
     security_rule {
         name                                  = "test-rule"
         priority                              = 1001
