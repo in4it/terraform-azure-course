e6acf056da5c0129322dc9da8e828c61184d7c06
Edward Viaene <ward@in4it.io>
2020-07-16 09:30:51 +0200

    azurerm bump

--- network-security-groups/main.tf
+++ network-security-groups/main.tf
@@ -1,4 +1,4 @@
 provider "azurerm" {
-  version = "=2.5.0"
+  version = "=2.18.0"
   features {}
 }
--- network-security-groups/network.tf
+++ network-security-groups/network.tf
@@ -10,7 +10,7 @@ resource "azurerm_subnet" "demo-internal-1" {
   name                 = "${var.prefix}-internal-1"
   resource_group_name  = azurerm_resource_group.demo.name
   virtual_network_name = azurerm_virtual_network.demo.name
-  address_prefix       = "10.0.0.0/24"
+  address_prefixes     = ["10.0.0.0/24"]
 }
 
 resource "azurerm_network_security_group" "allow-ssh" {
