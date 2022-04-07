bb7c1950fab344a1e75cdaacfec6bcf8377b0bfd
nathan <nathan@nksdigital.com>
2022-04-07 16:45:45 +0100

    updated IP address

--- first-steps/network.tf
+++ first-steps/network.tf
@@ -3,14 +3,14 @@ resource "azurerm_virtual_network" "demo" {
   name                = "${var.prefix}-network"
   location            = var.location
   resource_group_name = azurerm_resource_group.demo.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.150.0.0/16"]
 }
 
 resource "azurerm_subnet" "demo-internal-1" {
   name                 = "${var.prefix}-internal-1"
   resource_group_name  = azurerm_resource_group.demo.name
   virtual_network_name = azurerm_virtual_network.demo.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["10.150.0.0/24"]
 }
 
 resource "azurerm_network_security_group" "allow-ssh" {
