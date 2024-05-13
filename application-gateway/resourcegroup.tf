425002a4e80940049aadf5b470df799163370e71
Bladeoner <bladeoner112@gmail.com>
2024-05-13 12:32:42 +0200

    Changed deprecated application gateway sku size to v2

--- application-gateway/resourcegroup.tf
+++ application-gateway/resourcegroup.tf
@@ -1,4 +1,12 @@
+resource "random_string" "random-name" {
+  length  = 10
+  upper   = false
+  lower   = true
+  numeric = true
+  special = false
+}
+
 resource "azurerm_resource_group" "demo" {
-  name     = "application-gateway-demo"
+  name     = "application-gateway-demo-${random_string.random-name.result}"
   location = var.location
 }
