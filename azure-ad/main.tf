b4a17beb0d14f73537604f7778632804a3c7df25
Bladeoner <bladeoner112@gmail.com>
2024-07-11 11:16:34 +0200

    Added latest versions of the providers

--- azure-ad/main.tf
+++ azure-ad/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.70.0"
+      version = ">=3.111.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = ">= 2.41.0"
+      version = ">=2.53.0"
     }
   }
 }
