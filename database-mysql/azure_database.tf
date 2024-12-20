aee2864674f6fa40f9d66fd3f5e9c34cb4396cff
Edward Viaene <ward@in4it.io>
2024-12-20 15:46:41 -0600

    move to azurerm_mysql_flexible_server (#31)
    * move to azurerm_mysql_flexible_server
    
    * fix networking, sku, update readme

--- database-mysql/azure_database.tf
+++ database-mysql/azure_database.tf
@@ -1,46 +1,32 @@
-resource "azurerm_mysql_server" "demo" {
+resource "azurerm_mysql_flexible_server" "demo" {
   name                = "mysql-training"
   location            = azurerm_resource_group.demo.location
   resource_group_name = azurerm_resource_group.demo.name
 
-  sku_name = "GP_Gen5_2"
+  sku_name = "B_Standard_B1ms"
 
-  storage_mb                   = 5120
-  backup_retention_days        = 7
-  geo_redundant_backup_enabled = false
+  administrator_login    = "mysqladmin"
+  #administrator_password = "SETSECUREPASS"
 
-  administrator_login = "mysqladmin"
-  #administrator_login_password = "SETSECUREPASS"
-  version                 = "5.7"
-  ssl_enforcement_enabled = true
+  delegated_subnet_id    = azurerm_subnet.demo-database-1.id
+  private_dns_zone_id    = azurerm_private_dns_zone.demo.id
+
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.demo]
+
 }
 
-resource "azurerm_mysql_database" "training" {
+resource "azurerm_mysql_flexible_database" "demo" {
   name                = "demodb"
   resource_group_name = azurerm_resource_group.demo.name
-  server_name         = azurerm_mysql_server.demo.name
+  server_name         = azurerm_mysql_flexible_server.demo.name
   charset             = "utf8"
   collation           = "utf8_unicode_ci"
 }
 
-resource "azurerm_mysql_virtual_network_rule" "demo-database-subnet-vnet-rule" {
-  name                = "mysql-vnet-rule"
-  resource_group_name = azurerm_resource_group.demo.name
-  server_name         = azurerm_mysql_server.demo.name
-  subnet_id           = azurerm_subnet.demo-database-1.id
-}
-
-resource "azurerm_mysql_virtual_network_rule" "demo-subnet-vnet-rule" {
-  name                = "mysql-demo-subnet-vnet-rule"
-  resource_group_name = azurerm_resource_group.demo.name
-  server_name         = azurerm_mysql_server.demo.name
-  subnet_id           = azurerm_subnet.demo-internal-1.id
-}
-
-resource "azurerm_mysql_firewall_rule" "demo-allow-demo-instance" {
+resource "azurerm_mysql_flexible_server_firewall_rule" "demo-allow-demo-instance" {
   name                = "mysql-demo-instance"
   resource_group_name = azurerm_resource_group.demo.name
-  server_name         = azurerm_mysql_server.demo.name
+  server_name         = azurerm_mysql_flexible_server.demo.name
   start_ip_address    = azurerm_network_interface.demo-instance.private_ip_address
   end_ip_address      = azurerm_network_interface.demo-instance.private_ip_address
 }
